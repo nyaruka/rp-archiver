ddd0d47865e1f50ef28f2910a59d9665d442f628
Nic Pottier <nicpottier@gmail.com>
2018-06-07 14:07:38 -0500

    add test contact run, add recent run

--- archiver.go
+++ archiver.go
@@ -489,7 +489,7 @@ SELECT rec.visibility, row_to_json(rec) FROM (
 	  mm.created_on as created_on,
 	  sent_on
 	FROM msgs_msg mm JOIN contacts_contacturn ccu ON mm.contact_urn_id = ccu.id JOIN orgs_org oo ON ccu.org_id = oo.id
-	  JOIN LATERAL (select uuid, name from contacts_contact cc where cc.id = mm.contact_id and cc.is_test = FALSE) as contact ON True
+	  JOIN LATERAL (select uuid, name from contacts_contact cc where cc.id = mm.contact_id AND cc.is_test = FALSE) as contact ON True
 	  LEFT JOIN LATERAL (select uuid, name from channels_channel ch where ch.id = mm.channel_id) as channel ON True
 	  LEFT JOIN LATERAL (select coalesce(jsonb_agg(label_row), '[]'::jsonb) as data from (select uuid, name from msgs_label ml INNER JOIN msgs_msg_labels mml ON ml.id = mml.label_id AND mml.msg_id = mm.id) as label_row) as labels_agg ON True
 
@@ -1398,12 +1398,6 @@ func DeleteArchivedRuns(ctx context.Context, config *Config, db *sqlx.DB, s3Clie
 		err = executeInQuery(ctx, tx, deleteActionLogs, batchIDs)
 		if err != nil {
 			return fmt.Errorf("error removing action logs: %s", err.Error())
-		}
-
-		// then any labels
-		err = executeInQuery(ctx, tx, deleteMessageLabels, batchIDs)
-		if err != nil {
-			return fmt.Errorf("error removing message labels: %s", err.Error())
 		}
 
 		// any recent runs
