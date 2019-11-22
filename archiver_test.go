7829a6ebf36fbf9eb35e31df7c66a7f1df43d095
Rowan Seymour <rowanseymour@gmail.com>
2019-11-22 14:48:27 -0500

    Convert to github actions

--- archiver_test.go
+++ archiver_test.go
@@ -19,7 +19,7 @@ func setup(t *testing.T) *sqlx.DB {
 	testDB, err := ioutil.ReadFile("testdb.sql")
 	assert.NoError(t, err)
 
-	db, err := sqlx.Open("postgres", "postgres://localhost/archiver_test?sslmode=disable&TimeZone=UTC")
+	db, err := sqlx.Open("postgres", "postgres://temba:temba@localhost:5432/archiver_test?sslmode=disable&TimeZone=UTC")
 	assert.NoError(t, err)
 
 	_, err = db.Exec(string(testDB))
